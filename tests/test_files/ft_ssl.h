ad60eeca3ce016892ce7a1227c86b6512f046314
Leo SIMON <lsimon@e2r5p4.42.fr>
2019-12-20 10:49:27 +0100

    endgame: normed with headers

--- tests/test_files/ft_ssl.h
+++ tests/test_files/ft_ssl.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ssl.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsimon <lsimon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/20 10:47:01 by lsimon            #+#    #+#             */
+/*   Updated: 2019/12/20 10:47:01 by lsimon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_SSL_H
 
 # define FT_SSL_H
@@ -11,18 +23,21 @@
 # include <fcntl.h>
 # include <errno.h>
 
-/*
-** takes a string and return the hashed string
-*/
-typedef char			*(*t_hash_fn)(char *);
-typedef char			*(*t_parse_msg_fn)(char *);
+typedef struct			s_content
+{
+	size_t	size;
+	void	*content;
+}						t_content;
 
-typedef enum	e_command_type
+typedef char			*(*t_hash_fn)(t_content *);
+typedef t_content		*(*t_parse_msg_fn)(char *);
+
+typedef enum			e_command_type
 {
 	C_STANDARD,
 	C_CIPHER,
 	C_MDIGEST
-}				t_command_type;
+}						t_command_type;
 
 typedef struct			s_hash_handler
 {
@@ -31,12 +46,12 @@ typedef struct			s_hash_handler
 	t_command_type	type;
 }						t_hash_handler;
 
-typedef enum	e_type
+typedef enum			e_type
 {
 	H_STDIN,
 	H_STRING,
 	H_FILE
-}				t_type;
+}						t_type;
 
 typedef struct			s_process
 {
@@ -82,6 +97,61 @@ typedef struct			s_parser_handler
 }						t_parser_handler;
 
 /*
+** MD5 Structs
+*/
+
+typedef struct			s_md5_tools
+{
+	uint8_t			*s;
+	size_t			s_len;
+	uint32_t		aa;
+	uint32_t		bb;
+	uint32_t		cc;
+	uint32_t		dd;
+	uint32_t		a;
+	uint32_t		b;
+	uint32_t		c;
+	uint32_t		d;
+
+}						t_md5_tools;
+
+static uint32_t			g_k_part[64] =
+{
+	0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+	0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+	0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+	0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+	0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+	0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+	0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+	0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+	0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+	0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+	0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+	0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+	0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+	0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+	0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+	0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+};
+
+static uint32_t			g_s_part[64] =
+{
+	7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+	5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
+	4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+	6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
+};
+
+static uint32_t			g_m_part[64] =
+{
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+	1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12,
+	5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2,
+	0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9
+};
+
+/*
 ** Flags
 */
 void					quiet_mode(t_handler *handler);
@@ -90,16 +160,28 @@ void					reversed_mode(t_handler *handler);
 t_flag_fn				get_flag_fn(char c);
 int						string_mode(t_handler *handler, char *s);
 
-char					*hash_md5(char *s);
-char					*hash_sha256(char *s);
+char					*hash_md5(t_content *content);
+char					*hash_sha256(t_content *content);
 
-char					*get_content(int fd);
+/*
+** Content
+*/
 
-char					*handle_file(char *filepath);
-char					*handle_string(char *s);
-char					*handle_stdin(char *s);
+t_content				*init_content(void *input, size_t len);
+t_content				*get_content(int fd);
+void					free_content(t_content *content);
 
-t_handler				*init_handler(int ac, char **av);
+/*
+** Input
+*/
+
+t_content				*handle_file(char *filepath);
+t_content				*handle_string(char *s);
+t_content				*handle_stdin(char *s);
+t_parse_msg_fn			get_msg_fn(t_type type, unsigned int i);
+
+t_handler				*init_handler(char **av);
+void					free_handler(t_handler *handler);
 int						handle_flags(t_handler *handler, char **args);
 t_hash_fn				get_hash_fn(char *hash);
 
@@ -114,13 +196,15 @@ uint32_t				rot_right(uint32_t a, size_t b);
 /*
 ** Processes
 */
-t_process				*push_process(t_process *h, char *input, t_type type);
-t_process				*prepend_process(t_process *h, char *i, t_type type);
+t_process				*push_process(t_process *h, char *s, t_type t);
+t_process				*prepend_process(t_process *h, char *s, t_type t);
 void					run_processes(t_handler *handler, t_process *head);
+void					free_processes(t_process *head);
 
 void					handle_files(t_handler *handler, char **filespath);
 
-void					display(t_handler *h, t_process *p, char *ha, char *th);
+void					display(t_handler *h, t_process *p,
+								char *s, t_content c);
 
 /*
 ** Usage
@@ -128,6 +212,21 @@ void					display(t_handler *h, t_process *p, char *ha, char *th);
 void					arg_missing(void);
 void					arg_invalid_command(char *command);
 void					available_commands(void);
+
+/*
+** Sha256
+*/
+void					sha256_compress(uint32_t *w, uint32_t h[],
+							uint32_t k[]);
+t_mem					sha256_pad(t_content *c);
+
+/*
+** MD5
+*/
+void					init(t_md5_tools *md5);
+uint32_t				left_rotate(uint32_t elem1, uint32_t ele);
+void					add_save_values(t_md5_tools *md5);
+void					fix_endian(t_md5_tools *md5);
 
 static t_hash_handler	g_hash_table[] =
 {
